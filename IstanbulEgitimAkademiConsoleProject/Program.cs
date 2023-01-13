3a0fee3104ca6af7c13f931ac7c36835fb2d801a
Songül YAMAN BAYIK <snglymn@gmail.com>
2023-01-13 18:12:20 +0300

    Add project files.

---
+++ IstanbulEgitimAkademiConsoleProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IstanbulEgitimAkademiConsoleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i<1000)
+            {
+                Console.WriteLine(i + " Merhaba Ankara");
+                i++;
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
